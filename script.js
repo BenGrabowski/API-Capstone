c84d12d987652327f50963b41a6904ec83b99fb5
Ben Grabowski <gbowski30@gmail.com>
2019-06-10 21:51:39 -0600

    implemented getMetroID and getConcerts

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const songkickApiKey = '';
+const songkickApiKey = 'ZtmmBiNtoDue1K6l';
 const spotifyClientId = '6d0a3f2dec5a466b8d8744b0376d8e1f';
 const spotifyClientSecret = '3be3513498f6445ea007cacefa3df2cd';
 
 const songkickLocationBase = 'https://api.songkick.com/api/3.0/search/locations.json?';
 const spotifySearchBase = 'https://api.spotify.com/v1/search';
+const spotifyAuthorizeURL = 'https://accounts.spotify.com/authorize';
 
 //Get metro ID using location API
 function getMetroID(city, startDate, endDate) {
+    console.log('getMetroID ran');
     const params = {
-        // apikey = songkickApiKey,
-        q: city
+        apikey: songkickApiKey,
+        query: city
     };
     const queryString = formatParams(params);
     const url = songkickLocationBase + queryString;
@@ -22,6 +24,7 @@ function getMetroID(city, startDate, endDate) {
             return response.json();
         } throw new Error(response.statusText)
     })
+    // .then(responseJson => console.log(responseJson.resultsPage.results.location[0].metroArea.id, startDate, endDate))
     .then(responseJson => getConcerts(responseJson.resultsPage.results.location[0].metroArea.id, startDate, endDate))
     .catch(error => {
         $('#error').text(`Something Went Wrong: ${error.message}`);
@@ -31,15 +34,18 @@ function getMetroID(city, startDate, endDate) {
 
 //Get list of concerts from Songkick
 function getConcerts(metroID, startDate, endDate) {
+    console.log('getConcerts ran');
+    console.log(metroID, startDate, endDate);
     const params = {
         apikey: songkickApiKey,
-        metro_area_id: metroID,
         min_date: startDate,
         max_date: endDate
     };
 
-    queryString = formatParams(params);
-    const url = `https://api.songkick.com/api/3.0/metro_areas/${metroID}/calendar.json?` + queryString;
+    const queryString = formatParams(params);
+    console.log(queryString);
+    let url = `https://api.songkick.com/api/3.0/metro_areas/${metroID}/calendar.json?` + queryString;
+    console.log(url);
 
     //call fetch here with url
     fetch(url)
@@ -48,7 +54,8 @@ function getConcerts(metroID, startDate, endDate) {
             return response.json();
         } throw new Error(response.statusText);
     })
-    .then(responseJson => displayConcerts(responseJson))
+    .then(responseJson => console.log(responseJson))
+    // .then(responseJson => displayConcerts(responseJson))
     .catch(error => {
         $('#error').text(`Something Went Wrong: ${error.message}`);
     });
@@ -56,6 +63,7 @@ function getConcerts(metroID, startDate, endDate) {
 
 //Format the params into the query string
 function formatParams(params) {
+    console.log('formatParams ran');
     const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
@@ -99,27 +107,55 @@ function getArtistURI(artistName){
     })
     .then(responseJson => generatePlayer(responseJson.artists.items.id))
     .catch(error => {
-        $('#error').text(`Something Went Wrong ${error.message}`);
+        $('#error').text(`Something Went Wrong: ${error.message}`);
     });
 }
 
 //use client credentials flow to get access_token required for call to Search API
+// function getAccessToken(){
+//     $.ajax({
+//         type: "POST",
+//         url: "https://accounts.spotify.com/api/token",
+//         beforeSend: function(xhr) {
+//             xhr.setRequestHeader("Authorization", "Basic NmQwYTNmMmRlYzVhNDY2YjhkODc0NGIwMzc2ZDhlMWY6M2JlMzUxMzQ5OGY2NDQ1ZWEwMDdjYWNlZmEzZGYyY2Q=");
+//         },
+//         data: {grant_type: 'client_credentials'},
+//         error: function(xhr, error) {
+//             console.log(error.message);
+//         },
+//         success: function(data) {
+//             let response = data.json();
+//             let accessToken = response.access_token;
+//             return accessToken;
+//         }
+//     });
+// }
+
+//alternative version of call for access token
 function getAccessToken(){
-    $.ajax({
-        type: "POST",
-        url: "https://accounts.spotify.com/api/token",
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", "Basic " + 'NmQwYTNmMmRlYzVhNDY2YjhkODc0NGIwMzc2ZDhlMWY6IDNiZTM1MTM0OThmNjQ0NWVhMDA3Y2FjZWZhM2RmMmNk');
+   const url = 'https://accounts.spotify.com/api/token';
+   const data = {
+       'grant_type': 'client_credentials'
+   };
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic "NmQwYTNmMmRlYzVhNDY2YjhkODc0NGIwMzc2ZDhlMWY6M2JlMzUxMzQ5OGY2NDQ1ZWEwMDdjYWNlZmEzZGYyY2Q=',
+            // 'Content-Type': 'application/x-www-form-urlencoded'
+            // 'Access-Control-Request-Headers': 'Authorization', 'Content-Type'
         },
-        data: {grant_type: client_credentials},
-        error: function(xhr, error) {
-            console.log(error.message);
-        },
-        success: function(data) {
-            let response = data.json();
-            let accessToken = response.access_token;
-            return accessToken;
-        }
+        body: data
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } throw new Error(response.statusText);
+    })
+    // .then(responseJson => getArtistURI(responseJson))
+    // .then(responseJson => {return responseJson})
+    .then(responseJson => console.log(responseJson))
+    .catch(error => {
+        $('#error').text(`Something went wrong: ${error.message}`);
     });
 }
 
