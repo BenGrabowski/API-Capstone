61748c06529448a106a668d493a5a4394234c3b4
Ben Grabowski <gbowski30@gmail.com>
2019-06-20 13:53:41 -0600

    started styling

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function displayMap(fullAddress) {
     console.log(mapsEmbedUrl);
     
     let mapEl = 
-        `<iframe width="600" height="450" frameborder="0" style="border:0"
+        `<iframe width="600" height="337.5" frameborder="0" style="border:0"
         src="${mapsEmbedUrl}" allowfullscreen>
         </iframe>`
     $('#map').append(mapEl);
